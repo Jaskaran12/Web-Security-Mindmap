cad8e80a2802c0107bdf483ace883cb39528182a
jaskran12 <jaskaran1.dce@gmail.com>
2021-06-13 20:25:16 +0530

    first commit

---
+++ public/js/color.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const themeStylesheet = document.getElementById('theme');
+    const themeToggle = document.getElementById('theme-toggle');
+    themeToggle.addEventListener('click', () => {
+        // if it's light -> go dark
+        if(themeStylesheet.href.includes('light')){
+            themeStylesheet.href = 'dark-theme.css';
+            themeToggle.innerText = 'Switch to light mode';
+        } else {
+            // if it's dark -> go light
+            themeStylesheet.href = 'light-theme.css';
+            themeToggle.innerText = 'Switch to dark mode';
+
+        }
+    })
+})
